d6eb2e3800610c63aaa7a76fda66c2bfb80b5b9e
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-10-21 18:49:45 -0600

    Add placeholder stats table

--- src/components/FlightStats.vue
+++ src/components/FlightStats.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>Flight Stats</div>
+  <div class="ui equal width center aligned padded grid">
+    <div class="row">
+      <div class="blue column">Flight Stats</div>
+    </div>
+    <div class="blue row">
+      <div class="column">Altitude:</div>
+      <div class="column">Climb Rate:</div>
+    </div>
+    <div class="blue row">
+      <div class="column">AGL:</div>
+      <div class="column">Distance:</div>
+    </div>
+  </div>
 </template>
 
 <script>
