40d217cde4787ac087821351c6ef95fd4b662dc5
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-10-22 10:01:55 -0600

    Add file input and progress bar

--- src/components/FlightList.vue
+++ src/components/FlightList.vue
@@ -3,7 +3,21 @@
     <table class="ui celled striped table">
       <thead>
         <tr>
-          <th colspan="3">Flight List</th>
+          <th colspan="3">
+            <h3>Flight List</h3>
+            <div v-if="selectedFiles" class="ui indicating progress">
+              <div class="bar">
+                <div class="progress"></div>
+              </div>
+              <div class="label">{{ message }}</div>
+            </div>
+            <div v-else>
+              <label class="ui left icon input">
+                <input type="file" ref="file" @change="selectFile" />
+                <i class="file icon"></i>
+              </label>
+            </div>
+          </th>
         </tr>
       </thead>
       <tbody>
@@ -23,8 +37,51 @@
 </template>
 
 <script>
+import UploadService from "../services/TrackUpload";
+
 export default {
   name: "FlightList",
+  data() {
+    return {
+      selectedFiles: undefined,
+      currentFile: undefined,
+      progress: 0,
+      message: "Uploading",
+      fileInfos: [],
+    };
+  },
+  methods: {
+    selectFile() {
+      this.selectedFiles = this.$refs.file.files;
+    },
+    upload() {
+      this.progress = 0;
+
+      this.currentFile = this.selectedFiles.item(0);
+      UploadService.upload(this.currentFile, (event) => {
+        this.progress = Math.round((100 * event.loaded) / event.total);
+      })
+        .then((response) => {
+          this.message = response.data.message;
+          return UploadService.getFiles();
+        })
+        .then((files) => {
+          this.fileInfos = files.data;
+        })
+        .catch(() => {
+          this.progress = 0;
+          this.message = "Could not upload file";
+          this.currentFile = undefined;
+        });
+
+      this.selectedFiles = undefined;
+    },
+  },
+  mounted() {
+    UploadService.getFiles().then((response) => {
+      this.fileInfos = response.data;
+    });
+  },
 };
 </script>
 
